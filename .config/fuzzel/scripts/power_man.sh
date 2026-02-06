aee20572cf193f7cda704fe60cacb855b508d230
felix <kaszynskibolbo@gmail.com>
2026-02-06 09:52:58 +0200

    fixed the issue with fuzzel window size, addded keybind in hyprland config

--- .config/fuzzel/scripts/power_man.sh
+++ .config/fuzzel/scripts/power_man.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-chosen=$(printf "Shutdown\nRestart\nLock" | fuzzel -d)
+chosen=$(printf "Shutdown\nRestart\nLock" | fuzzel -d -l 3)
 
 case "$chosen" in
         "Shutdown") shutdown now ;;
